e3f6074304408fdafeaff9fd487bc642acce7729
6369973055 <126741840+6369973055@users.noreply.github.com>
2023-03-02 14:02:57 +0530

    Add files via upload

---
+++ vowles.py
@@ -0,0 +1,12 @@
+"""n=input("enter the statment:")
+v="aeiou"
+vc=0
+for i in n:
+    if (i in v):
+        vc+=1
+print("number of vc=",vc)"""
+
+def cv(n):
+    return 5*(4**(n-1))
+n=int(input("enter the number"))
+print(cv(n))
